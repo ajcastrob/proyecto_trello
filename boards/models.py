0457e61f9e1a9405e0a14d95abf95349daed9ffe
José Castro <a.j.castro.b@gmail.com>
2026-08-08 14:25:53 -0500

    ✨ [ADD] Models
    Adding models Task and TaskList

--- boards/models.py
+++ boards/models.py
@@ -17,3 +17,27 @@ class Board(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class TaskList(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="lists")
+    title = models.CharField(max_length=120)
+    position = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.board.title} - {self.title}"
+
+
+class Task(models.Model):
+    task_list = models.ForeignKey(
+        TaskList, on_delete=models.CASCADE, related_name="tasks"
+    )
+    title = models.CharField(verbose_name="título", max_length=200)
+    description = models.TextField(verbose_name="descripción", blank=True)
+    position = models.PositiveIntegerField(verbose_name="posición", default=0)
+    created_at = models.DateTimeField(
+        verbose_name="Fecha de creación", auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.title
